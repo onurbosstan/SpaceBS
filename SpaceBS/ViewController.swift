342f76d290ba01748820f06e5eb525924422c343
Onur Bostan <onurbosstan@gmail.com>
2024-01-31 18:57:18 +0300

    Initial Commit

---
+++ SpaceBS/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SpaceBS
+//
+//  Created by ONUR BOSTAN on 31.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var list = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
