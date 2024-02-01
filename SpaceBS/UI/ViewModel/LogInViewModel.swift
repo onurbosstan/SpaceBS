9e9bba8a2f76de02b52d3cb9ce895c9bf2c1440a
Onur Bostan <onurbosstan@gmail.com>
2024-02-01 22:15:44 +0300

    added rocket animation

---
+++ SpaceBS/UI/ViewModel/LogInViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LogInViewModel.swift
+//  SpaceBS
+//
+//  Created by ONUR BOSTAN on 1.02.2024.
+//
+
+import Foundation
--- SpaceBS/ViewControllers/LogInViewController.swift
+++ SpaceBS/ViewControllers/LogInViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LogInViewController: UIViewController {
     
     var list = ""
 
