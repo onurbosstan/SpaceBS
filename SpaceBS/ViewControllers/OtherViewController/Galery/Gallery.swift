8f8e701b0803e3292886eb7c92593816c3766162
Onur Bostan <onurbosstan@gmail.com>
2024-02-27 16:46:48 +0300

    fixed collectionView encodings on gallery page

--- SpaceBS/ViewControllers/OtherViewController/Galery/Gallery.swift
+++ SpaceBS/ViewControllers/OtherViewController/Galery/Gallery.swift
@@ -13,11 +13,12 @@ class Gallery: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     private let viewModel = GalleryViewModel()
     private let disposeBag = DisposeBag()
+    var photoModel = [NASAItem]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.delegate = nil
-        collectionView.dataSource = nil
+        collectionView.delegate = self
+        collectionView.dataSource = self
         collectionView.collectionViewLayout = UICollectionViewLayout()
         collectionView.collectionViewLayout = CuriosityCollectionLayout(colmnsNumber: 2, minColmnsNumber: 1, minCell: 1)
         Animation.showActivityIndicator()
@@ -26,33 +27,41 @@ class Gallery: UIViewController {
         viewModel.fetchData(query: "Galaxy")
     }
     private func bindViewModel() {
-        viewModel.nasaItems
-            .observe(on: MainScheduler.instance)
-            .bind(to: collectionView.rx.items(cellIdentifier: "GalleryCell", cellType: GalleryCell.self)) { row, item, cell in
-                if let imageUrlString = item.links.first?.href, let imageUrl = URL(string: imageUrlString) {
-                    cell.imageView.kf.setImage(with: imageUrl)
-                }
-            }
-            .disposed(by: disposeBag)
-
-        viewModel.isLoading
-            .observe(on: MainScheduler.instance)
-            .subscribe(onNext: { [weak self] isLoading in
-                if isLoading {
-                    Animation.showActivityIndicator()
-                } else {
+            viewModel.nasaItems
+                .observe(on: MainScheduler.instance)
+                .subscribe(onNext: { [weak self] items in
+                    self?.photoModel = items
+                    self?.collectionView.reloadData()
                     Animation.hideActivityIndicator()
-                }
-            })
-            .disposed(by: disposeBag)
+                })
+                .disposed(by: disposeBag)
     }
 }
 
 extension Gallery: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return photoModel.count
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GalleryCell", for: indexPath) as? GalleryCell else {
+            return UICollectionViewCell()
+        }
+        let photoItem = photoModel[indexPath.item]
+        if let imageUrlString = photoItem.links.first?.href, let imageUrl = URL(string: imageUrlString) {
+            cell.imageView.kf.setImage(with: imageUrl)
+        }
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let selectedPhoto = photoModel[indexPath.row]
+        showGalleryDetail(with: selectedPhoto)
+    }
+    func showGalleryDetail(with item: NASAItem) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let galleryDetailVC = storyboard.instantiateViewController(withIdentifier: "GalleryDetail") as! GalleryDetail
+        galleryDetailVC.modalPresentationStyle = .overFullScreen
+        galleryDetailVC.modalTransitionStyle = .crossDissolve
+        galleryDetailVC.selectedItem = item
+        present(galleryDetailVC, animated: true, completion: nil)
     }
 }
